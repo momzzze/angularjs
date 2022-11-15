8b709e2d4cc7622d746d6ffb9d17e7ea3d473ee9
Nikola Ninov <momzzze@abv.bg>
2022-11-15 16:46:05 +0200

    some directives testing and learning

--- testing-learning/src/app/app.module.ts
+++ testing-learning/src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning - alert/warning-alert.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- testing-learning/src/app/servers/servers.component.ts
+++ testing-learning/src/app/servers/servers.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-servers',
+  // selector:`.app-servers`,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  username: string = '';
+  serverCreated = false;
+  servers = ['Test Server', 'Test Server 2'];
+  clickStatus = false;
 
-  constructor() { }
+  incrementingNumber: number = 0;
+  incrementingArr: number[] = [];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    }, 2000);
+  }
 
   ngOnInit(): void {
   }
-
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created ' + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onClickHandler() {
+    this.username = '';
+  }
+  isClicked() {
+    this.clickStatus = !this.clickStatus
+  }
+  incrementLog() {
+    this.incrementingNumber++;
+    this.incrementingArr.push(this.incrementingNumber);
+  }
 }
